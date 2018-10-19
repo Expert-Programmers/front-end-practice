3915f840836aaee50ff529c43cf37efce5557c85
ltoddy <taoliu14@acm.org>
2018-10-19 15:33:42 +0800

    Running an Express GraphQL Server

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,16 +1,25 @@
-const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
+// Construct a schema, using GraphQL schema language.
 const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+// The root provides a resolver function for each API endpoint.
 const root = {
   hello: () => 'Hello world!'
 };
 
-// Run the GraphQL query `{ hello }` and print out the response
-graphql(schema, '{ hello }', root).then(console.log);
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
