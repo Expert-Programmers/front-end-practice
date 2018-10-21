f46c7af9932b400676608ca75af55ae144a85295
ltoddy <taoliu14@acm.org>
2018-10-21 20:11:45 +0800

    Authentication & Middleware

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -2,62 +2,47 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language.
 const schema = buildSchema(`
-  input MessageInput {
-    content: String
-    author: String
-  }
-
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-
   type Query {
-    getMessage(id: ID!): String
-  }
-
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID! input: MessageInput): Message
+    ip: String
   }
 `);
 
-// If Message had any complex fields, wd'd put them on this object.
-class Message {
-  constructor(id, { content, author }) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
+function loggingMiddleware(request, response, next) {
+  console.log('ip:', request.ip);
+  next();
 }
 
-// Maps username to content
-const fakeDatabase = {};
-
 const root = {
-  getMessage: id => {
-    if (!fakeDatabase[id]) throw new Error('no message exists with id ' + id);
-    return new Message(id, fakeDatabase[id]);
-  },
-  createMessage: ({ input }) => {
-    // Create a random id for our "database"
-    const id = require('crypto').randomBytes(10).toString('hex');
-
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  updateMessage: ({ id, input }) => {
-    if (!fakeDatabase[id]) throw new Error('no message exists with id ' + id);
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  }
+  ip: (args, request) => request.ip
 };
 
 const app = express();
+app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
-  schema: schema, rootValue: root, graphiql: true
+  schema,
+  rootValue: root,
+  graphiql: true
 }));
-app.listen(4000, _ => console.log('Running a GraphQL API server at localhost:4000/graphql'));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+
+// const query = `
+//   query {
+//     ip
+//   }
+// `;
+//
+// fetch('/graphql', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     query
+//   }),
+// })
+//   .then(r => r.json())
+//   .then(data => console.log(data));
