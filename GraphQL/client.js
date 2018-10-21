be7e5471ab3605d459c87755d5313de8b68393e8
ltoddy <taoliu14@acm.org>
2018-10-21 19:59:09 +0800

    Mutations and Input Types

--- GraphQL/client.js
+++ GraphQL/client.js
@@ -1,18 +1,25 @@
-const sides = 6;
-const query = `{
-  getDie(numSides: 6) {
-    rollOnce,
-    roll(numRolls: 3)
+const author = 'andy';
+const content = 'hope is a good thing';
+const query = `
+  mutation CreateMessage($input: MessageInput) {
+    createMessage(input: $input) {
+      id
+    }
   }
-}`;
+`;
 
 fetch('/graphql', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'Accept': 'application/json',
+    'Accept': 'application/json'
   },
-  body: JSON.stringify({ query, variables: { sides } })
+  body: JSON.stringify({
+    query,
+    variables: {
+      input: { author, content }
+    }
+  })
 })
   .then(r => r.json())
   .then(data => console.log(data));
--- GraphQL/server.js
+++ GraphQL/server.js
@@ -4,45 +4,60 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language.
 const schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID! input: MessageInput): Message
   }
 `);
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({ numRolls }) {
-    let output = [];
-    for (let i = 0; i < numRolls; i++) output.push(this.rollOnce());
-    return output;
+// If Message had any complex fields, wd'd put them on this object.
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
 }
 
-// The root provides a resolver function for each API endpoint.
+// Maps username to content
+const fakeDatabase = {};
+
 const root = {
-  getDie: ({ numSides }) => new RandomDie(numSides)
+  getMessage: id => {
+    if (!fakeDatabase[id]) throw new Error('no message exists with id ' + id);
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({ input }) => {
+    // Create a random id for our "database"
+    const id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) throw new Error('no message exists with id ' + id);
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  }
 };
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true
+  schema: schema, rootValue: root, graphiql: true
 }));
-
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(4000, _ => console.log('Running a GraphQL API server at localhost:4000/graphql'));
