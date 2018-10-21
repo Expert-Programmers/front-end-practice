bf9741bde2c4060658b642cf1d765c0ae0e28367
ltoddy <taoliu14@acm.org>
2018-10-21 10:49:42 +0800

    enum

--- typescript/tutorial/advance.ts
+++ typescript/tutorial/advance.ts
@@ -23,6 +23,60 @@ function handleEvent(ele: Element, event: EventNames): void {
 handleEvent(document.getElementById('hello'), 'scroll');
 // handleEvent(document.getElementById('world'), 'dbclick');
 
+// 元组
+let name1: [string, number] = ['one', 10];
+let name2: [string, number];
+name2[0] = 'other';
+name2[1] = 200;
+
+name2[0].slice(1);
+name2[1].toFixed(2);
+
+let name3: [string, number];
+name3 = ['three', 20];
+
+// 枚举
+enum Days {Sun, Mon, Tue, Wed, Thu, Fri, Sat}
+
+console.log(Days["Sun"] === 0); // true
+console.log(Days["Mon"] === 1); // true
+console.log(Days["Tue"] === 2); // true
+console.log(Days["Sat"] === 6); // true
+
+console.log(Days[0] === "Sun"); // true
+console.log(Days[1] === "Mon"); // true
+console.log(Days[2] === "Tue"); // true
+console.log(Days[6] === "Sat"); // true
+
+enum Days2 {Sum = 3, Mon = 1, Tue, Wed, Thu, Fri, Sat}
+
+// 常数枚举
+const enum Directions {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
