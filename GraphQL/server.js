062a24213e8a8e2e3ab4c111492526ceff0d1b40
ltoddy <taoliu14@acm.org>
2018-10-22 15:05:17 +0800

    constructing types

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,48 +1,48 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const graphql = require('graphql');
 
-const schema = buildSchema(`
-  type Query {
-    ip: String
+// Maps id to User object
+const fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice'
+  },
+  'b': {
+    id: 'b',
+    name: 'bob'
   }
-`);
-
-function loggingMiddleware(request, response, next) {
-  console.log('ip:', request.ip);
-  next();
-}
-
-const root = {
-  ip: (args, request) => request.ip
 };
 
+// define User type
+const userType = graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString }
+  }
+});
+
+// define Query type
+const queryType = graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      args: {
+        id: { type: graphql.GraphQLString }
+      },
+      resolve: (_, args) => fakeDatabase[args.id]
+    }
+  }
+});
+
+const schema = new graphql.GraphQLSchema({ query: queryType });
+
 const app = express();
-app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
+  schema: schema,
   graphiql: true
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
-
-
-// const query = `
-//   query {
-//     ip
-//   }
-// `;
-//
-// fetch('/graphql', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Accept': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     query
-//   }),
-// })
-//   .then(r => r.json())
-//   .then(data => console.log(data));
