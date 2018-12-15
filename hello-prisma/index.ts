c7c53cfabe81013f5dc1eee044282d07feab5c7f
ltoddy <taoliu14@acm.org>
2018-12-15 19:18:21 +0800

    read and write nested objects

--- hello-prisma/index.ts
+++ hello-prisma/index.ts
@@ -1,14 +1,34 @@
-import { prisma } from "./generated/prisma-client";
+import { prisma, User } from "./generated/prisma-client";
 
+// A `main` function so that we can use async/await
 async function main() {
 
   // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+  // const newUser: User = await prisma.createUser({
+  //   name: "Bob",
+  //   email: "bob@prisma.io",
+  //   posts: {
+  //     create: {
+  //       title: "The data layer for modern apps"
+  //     }
+  //   }
+  // });
+  //
+  // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+  //
+  // Read all users from the database and print them to the console
+  // const allUsers = await prisma.users();
+  // console.log(allUsers);
+  //
+  // const allPosts = await prisma.posts();
+  // console.log(allPosts);
 
-  // Read all users from the database and print them on the console
-  const allUser = await prisma.users();
-  console.log(allUser);
+  // Read the previously created user from the database and print their posts to the console
+  const postByUser = await prisma
+    .user({ email: "bob@prisma.io" })
+    .posts();
+  console.log(`All posts by that user: ${JSON.stringify(postByUser)}`);
+
 }
 
 main().catch(e => console.error(e));
